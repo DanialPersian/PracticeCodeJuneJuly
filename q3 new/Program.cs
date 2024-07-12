e14eb5dbe4da10d685a75fd0bc2028703fd81088
Danial Rafi <rfidanial@gmail.com>
2024-07-12 17:05:53 +0100

    q3 bishbashbosh q

--- q3 new/Program.cs
+++ q3 new/Program.cs
@@ -1,53 +1,27 @@
-﻿using System;
+﻿int BingBangBONG = 0;
 
-class FizzWhizBuzzBong
+for(int i = 0; i < 100; i++)
 {
-    static void Main(string[] args) //here is the main method 
+    if(i % 3 == 0 && i % 5 == 0)
     {
-        // Define the range
-        int start = 1;
-        int end = 12;
-
-        // Initialize counters
-        int fizzwhizbuzzCount = 0;
-        int bongCount = 0;
-        int fizzCount = 0;
-        int fizzwhizCount = 0;
-
-        // Play the game for each number in the range
-        for (int i = start; i <= end; i++)
+        if(isString(i.ToString()) % 2 == 0)
         {
-            GetFizzWhizBuzzBong(i, ref fizzwhizbuzzCount, ref bongCount, ref fizzCount, ref fizzwhizCount);
+            BingBangBONG++;
         }
 
-        // Print the results
-        
-        Console.WriteLine($"Fizz: {fizzCount}");
-        Console.WriteLine($"FizzWhiz: {fizzwhizCount}");
-        Console.WriteLine($"FizzWhizBuzz: {fizzwhizbuzzCount}");
-        Console.WriteLine($"Bong: {bongCount}");
-    }
-
-    static void GetFizzWhizBuzzBong(int x, ref int fizzwhizbuzz, ref int bong, ref int fizz, ref int fizzwhiz)
-    {
-        if (x % 8 == 0)
-        {
-            fizzwhizbuzz++;
-        }
-        else if (x % 4 == 0)
-        {
-            fizzwhiz++;
-        }
-        else if (x % 2 == 0)
-        {
-            fizz++;
-        }
-        else
-        {
-            bong++;
-        }
     }
 }
 
-//If the largest number amongst {2,4,8} which can divide x is "2,"  then we count it as an instance of "Fizz."
-//meaning that out of 2,4,8 the number that can divide it is 2 then its fizz - doesn't mean if all of them can divide it , meaning if just 2 can divide it out of all of them 
+Console.WriteLine(BingBangBONG);
+//had to make num a string because I can't iterate through an int and characters can only go through strings but i am turning it back into an int with the function of -'0'
+int isString(string num)
+{
+    int isString = 0;
+    foreach (char c in num)
+    {
+        
+        return isString += c-'0';
+    }
+    return isString;
+}
+
