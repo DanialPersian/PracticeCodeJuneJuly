6b20c634db88b5f1f039230c7838f7accb5712d6
Danial Rafi <rfidanial@gmail.com>
2024-07-13 15:44:59 +0100

    working on file readinf as it was incorrect

--- Q6/Program.cs
+++ Q6/Program.cs
@@ -1,32 +1,36 @@
-﻿//get the lines out of the files 
+﻿using System;
+using System.IO;
 
-string[] lines = File.ReadAllLines(@"dogOwners.txt");
-
-//Dangelo Broomfield:[BRUISER:Blue,OTIS:Orange,KLARK:Black,NEVE:White,HENRY:Orange]
-//     1                2     3    4     5     6      7    8    9      10   11
-
-int amount = 0;
-
-foreach (string line in lines)
+class Program
 {
-    string[] values = line.Split(new char[] { ':', '[', ',', ']' }, StringSplitOptions.RemoveEmptyEntries);
-
-    // Initialize the red collar count
-    int redCollarCount = 0;
-
-    // Start from index 2 and step by 2 to get all the collar colors
-    for (int i = 2; i < values.Length; i += 2)
+    static void Main()
     {
-        if (values[i] == "Red")
+        string[] lines = File.ReadAllLines(@"dogOwners2.txt");
+        int amount = 0;
+
+        foreach (string line in lines)
         {
-            redCollarCount++;
-        }
-    }
+            string[] values = line.Split(new char[] { ':', '[', ',', ']' }, StringSplitOptions.RemoveEmptyEntries);
+            int redCollarCount = 0;
 
-    if (redCollarCount >= 2)
-    {
-        Console.WriteLine(line);
-        amount++;
+            
+            for (int i = 2; i < values.Length; i += 2)
+            {
+                string collarColor = values[i].Trim(); 
+
+                if (collarColor == "Red")
+                {
+                    redCollarCount++;
+                }
+            }
+
+            if (redCollarCount >= 2)
+            {
+                Console.WriteLine(line);
+                amount++;
+            }
+        }
+
+        Console.WriteLine(amount);
     }
-}
-Console.WriteLine(amount);
+}s
--- q4/Program.cs
+++ q4/Program.cs
@@ -1,27 +1,22 @@
-﻿int BingBangBONG = 0;
+﻿int BishBashBosh = 0;
+int sum = 0;
+
 
 for (int i = 1000; i <= 50000; i++)
 {
-    bool divisibleBy3 = (i % 3 == 0);
-    bool divisibleBy5 = (i % 5 == 0);
-
-    if (divisibleBy3 && divisibleBy5)
+    if(i % 3 == 0 && i % 5 == 0)
     {
-        if (isEvenDigitSum(i))
+        foreach(char c in (i.ToString()))
         {
-            BingBangBONG++;
+            
+            sum += c - '0';
+           
+        }
+        if (sum % 2 == 0)
+        {
+            BishBashBosh++;
         }
     }
 }
 
-Console.WriteLine(BingBangBONG);
-
-bool isEvenDigitSum(int num)
-{
-    int sumOfDigits = 0;
-    foreach (char c in num.ToString())
-    {
-        sumOfDigits += c - '0';
-    }
-    return sumOfDigits % 2 == 0;
-}
+Console.WriteLine(BishBashBosh);
