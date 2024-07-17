d75ecded4f65d9254e5b8b01e1b5ac68debb1f96
Danial Rafi <rfidanial@gmail.com>
2024-07-17 20:19:12 +0100

    string splitting and file extraction but main focus rn is string splitting

--- Q2/Program.cs
+++ Q2/Program.cs
@@ -1,3 +1,31 @@
-﻿string words = "Danial,micheal,scott,human ";
+﻿using System.Drawing;
 
-Console.WriteLine(words.Split(','));
+
+//Lina Mills:[SATCHEL:Brown,NIKKI:Black,BUDD:Green]
+
+string[] lines= File.ReadAllLines(@"C:\Users\Dania\source\repos\PracticeCodeJune\Q2\dogOwners2.txt");
+
+foreach (string line in lines)
+{
+    string[] parts = line.Split(':');
+
+    string Oname = parts[0];
+
+    foreach (var a in Oname)
+    {
+        Console.WriteLine(a);
+    }
+}
+
+
+
+
+//char[] seperator = new char[] {':'};
+
+//foreach(var a in words.Split(seperator,StringSplitOptions.RemoveEmptyEntries))
+//{
+//    Console.WriteLine(a);
+//}
+
+
+
