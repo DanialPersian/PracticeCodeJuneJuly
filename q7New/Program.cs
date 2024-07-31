0578752fff5b86f0eba4fef65975d727ed74647d
Danial Rafi <rfidanial@gmail.com>
2024-07-31 18:39:47 +0100

    d

--- q7New/Program.cs
+++ q7New/Program.cs
@@ -1,12 +1,13 @@
 ﻿int[,] grid = { { 1, 6, 4 }, { 4, 3, 8 }, { 2, 5, 5 } };
+List<int> numbers = new List<int>();
 
-int[,] values = new int[3, 3]; // meaning 3 rows and 3 columns
 
-for(int rowIndex = 0; rowIndex < grid.GetLength(0); rowIndex++)
+
+for (int rowIndex = 0; rowIndex < grid.GetLength(0); rowIndex++)
 {
     for (int columnIndex = 0; columnIndex < grid.GetLength(1); columnIndex++)
     {
-        List<int> numbers = new List<int>();
+        
         numbers.Add(grid[rowIndex, columnIndex]);
     }
 }
