ca939650a08d3e16cb989da440eca6db30a3c74d
Danial Rafi <rfidanial@gmail.com>
2024-08-01 18:27:43 +0100

    finished q7 , finding largest in the middle

--- q7New/Program.cs
+++ q7New/Program.cs
@@ -1,4 +1,4 @@
-﻿int[,] grid = { { 1, 6, 4 }, { 4, 3, 8 }, { 2, 5, 5 } };
+﻿int[,] grid = { { 1, 6, 4, 9, 4 }, { 4, 3, 8, 5, 6 }, { 2, 5, 5, 4, 7 }, { 3, 4, 8, 5, 1 } };
 List<int> numbers = new List<int>();
 
 
@@ -10,6 +10,8 @@ for (int rowIndex = 0; rowIndex < grid.GetLength(0); rowIndex++)
         
         numbers.Add(grid[rowIndex, columnIndex]);
     }
+    int largestNumber = FindLargestNumberWithFourLargerNeighbours(grid);
+    Console.WriteLine("The largest number with four larger neighbors is: " + largestNumber);
 }
 
 static int FindLargestNumberWithFourLargerNeighbours(int[,] grid)
@@ -22,6 +24,19 @@ static int FindLargestNumberWithFourLargerNeighbours(int[,] grid)
 
     for(int i = 1; i < DanialRows - 1; i++)
     {
-
+        for (int k = 1; k < DanialColumns -1; k ++)
+        {
+            int DanialCurrentNumber = grid[i, k];
+            //checking if all four numbers are larger
+            if(grid[i - 1, k] > DanialCurrentNumber && grid[i + 1, k] > DanialCurrentNumber && grid[i, k - 1] > DanialCurrentNumber && grid[i, k + 1] > DanialCurrentNumber)
+            {
+                if(DanialCurrentNumber > maxNumber)
+                {
+                    maxNumber = DanialCurrentNumber;
+                }
+            }
+            
+        }
     }
+    return maxNumber;
 }
