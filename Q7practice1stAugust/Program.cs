10cc5c4f77eb2892bfeceae85ab8accde02b74ab
Danial Rafi <rfidanial@gmail.com>
2024-08-01 18:57:15 +0100

    d

---
+++ Q7practice1stAugust/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = new int[20];
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (i % 2 != 0)
+    {
+        array[i] = 1;
+    }
+}
+
+foreach(var item in array)
+{
+    Console.WriteLine(item);
+}
+//creates an array of size 20 and assign all the elements of the array that have an odd index to the value 1
+//creates an array of size 20 and assign all the elements of the array that have an odd index to the value 1
