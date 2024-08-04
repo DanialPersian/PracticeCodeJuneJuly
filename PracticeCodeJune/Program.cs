741a57dff239b05ecc6633a531f28507d6dd4cd0
Danial Rafi <rfidanial@gmail.com>
2024-08-04 19:57:18 +0100

    working on consonant quesiton

--- PracticeCodeJune/Program.cs
+++ PracticeCodeJune/Program.cs
@@ -1,7 +1,28 @@
-﻿int[] intarray = new int[10];
-int count = 0;
-while (count < 10)
+﻿string question = "ab1cd23ef4gh";
+
+char[] consonant = {'b', 'c', 'd','f','g', 'h','j', 'k', 'l','m','n','p','q','r','s','t','v','w','x','y','z'};
+
+List<string> names = new List<string> { "b", "c"," d", "f"," g","h","j","k","l","m", "n"," p","q","r", "s","t","v","w", "x","y", "z" }; //started using a list now instead of an array but this time it is a character 
+
+int sum = 0;
+int answer = 0;
+
+
+foreach(char c in question)
 {
-    intarray[count] = 5;
-     = count + 2;
+    if (names.Contains(c))
+    {
+        sum++;   
+    }
+    answer += c-'0';
 }
+
+
+
+Console.WriteLine(answer*sum);
+
+
+string checker(string chcek)
+{
+    foreach(char c in chcek)
+    {
